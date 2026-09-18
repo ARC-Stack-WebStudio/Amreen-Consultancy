2e651e17751818585338ab932ad1fd43f3f55361
ARC Stack Web Studio <arcstackwebstudio@gmail.com>
2026-09-18 21:20:40 +0530

    New Image Added

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,9 +1,16 @@
-import headerLogo from '../assets/Logo/Amreen Blue Logo hortizontel.png';
-import headerLogoFull from '../assets/Logo/Amreen Blue Logo.png';
+import headerLogo from '../assets/Logo/AMREEN Logo.png';
+import headerLogoFull from '../assets/Logo/AMREEN Logo.png';
+
+import headerIconFull from '../assets/Logo/Amreen Icon.png';
+import headerIcon from '../assets/Logo/Amreen Icon.png';
 
 export default function Brand({ light = false, full = false }) {
   return (
     <div className={`brand ${light ? 'brand-light' : ''}`}>
+      <img
+        src={full ? headerIconFull : headerIcon}
+        alt="AMREEN CONSULTANCY - Overseas Recruitment"
+      />
       <img
         src={full ? headerLogoFull : headerLogo}
         alt="AMREEN CONSULTANCY - Overseas Recruitment"
